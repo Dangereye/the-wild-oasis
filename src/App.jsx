2f7ed62bfaf4af39c0df96502d2bd8afc60bc5d0
Craig Puxty <craig_puxty@talktalk.net>
2024-01-09 21:46:09 +0000

    Updated App & Button component styles. Added Input component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `;
 
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 export default function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
+      <StyledApp>
         <H1>The Wild Oasis</H1>
-      </div>
+        <Button>Check in</Button>
+        <Button>Check Out</Button>
+        <Input type='number' placeholder='Enter a number' />
+        <Input type='text' placeholder='Enter some text' />
+      </StyledApp>
     </>
   );
 }
