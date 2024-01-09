db83ac3030267ed6d71d0a764e8711145544cfe9
Craig Puxty <craig_puxty@talktalk.net>
2024-01-09 21:22:15 +0000

    Installed styled-components. Added GlobalStyles. Added custom-fonts to index & updated the page title.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>The Wild Oasis</H1>
+      </div>
+    </>
+  );
 }
