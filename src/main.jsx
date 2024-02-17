e57389591b857540803402ead672bcc08ac2a87e
Craig Puxty <craig_puxty@talktalk.net>
2024-02-17 14:12:56 +0000

    Installed react-error-boundary, updated main & ErrorFallback - error boundaries during renders.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
+// React
 import React from 'react';
+
+// React DOM
 import ReactDOM from 'react-dom/client';
+
+// React error boundary
+import { ErrorBoundary } from 'react-error-boundary';
+
+// Components
 import App from './App.jsx';
+import ErrorFallback from './ui/ErrorFallback.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace('/')}
+    >
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
